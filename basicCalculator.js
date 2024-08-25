2cf5ad26e86fe2072a59263c3a6e0a0a52a97ab7
Abigail Rick <abigail_rick@mailfence.com>
2024-08-25 05:54:57 -0500

    button type
    changed tyrpe of button to button

--- basicCalculator.js
+++ basicCalculator.js
@@ -1,1 +1,36 @@
+const AC = (() => document.getElementById('AC'))();
+AC.addEventListener('click', clearInputs);
+
+const input = document.getElementById('input');
+
+const positiveNegative = document.getElementById('positiveNegative');
+
+const percent = document.getElementById('percent');
+
+const divide = (() => document.getElementById('divide'))();
+const divideFunction = () => {
+    input.value / 
+
+}
+divide.addEventListener('click', )
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+
+        <button type="button" id="divide">/</button>
+        <button type="button" id="seven">7</button>
+        <button type="button" id="eight">8</button>
+        <button type="button" id="nine">9</button>
+        <button type="button" id="multiply">x</button>
+        <button type="button" id="four">4</button>
+        <button type="button" id="five">5</button>
+        <button type="button" id="six">6</button>
+        <button type="button" id="subtract">-</button>
+        <button type="button" id="one">1</button>
+        <button type="button" id="two">2</button>
+        <button type="button" id="three">3</button>
+        <button type="button" id="add">+</button>
+        <button type="button" id="one">1</button>
+        <button type="button" id="decimal">.</button>
+        <button type="button" id="equals">=</button>
 
