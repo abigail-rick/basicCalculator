66c64d5b8ab2d6fe8d0df266717605f664d28963
Abigail Rick <abigail_rick@mailfence.com>
2024-09-04 08:47:57 -0500

    add class change

--- basicCalculator.js
+++ basicCalculator.js
@@ -1,36 +1,29 @@
+const calculator = document.querySelectorAll('calculatorButton');
+
+function clearInputs() {
+    const input = document.getElementById("input");
+    input.value = '';
+}
+
 const AC = (() => document.getElementById('AC'))();
 AC.addEventListener('click', clearInputs);
 
-const input = document.getElementById('input');
+function addInputs(){
+    let sum = 0;
+    input.value += sum;
+}
+
+const add = (()=> document.getElementById('add'))();
+add.addEventListener('click', addInputs);
+
+
 
 const positiveNegative = document.getElementById('positiveNegative');
 
 const percent = document.getElementById('percent');
 
-const divide = (() => document.getElementById('divide'))();
-const divideFunction = () => {
-    input.value / 
-
-}
-divide.addEventListener('click', )
 const seven = document.getElementById('seven');
 const eight = document.getElementById('eight');
 const nine = document.getElementById('nine');
 
-        <button type="button" id="divide">/</button>
-        <button type="button" id="seven">7</button>
-        <button type="button" id="eight">8</button>
-        <button type="button" id="nine">9</button>
-        <button type="button" id="multiply">x</button>
-        <button type="button" id="four">4</button>
-        <button type="button" id="five">5</button>
-        <button type="button" id="six">6</button>
-        <button type="button" id="subtract">-</button>
-        <button type="button" id="one">1</button>
-        <button type="button" id="two">2</button>
-        <button type="button" id="three">3</button>
-        <button type="button" id="add">+</button>
-        <button type="button" id="one">1</button>
-        <button type="button" id="decimal">.</button>
-        <button type="button" id="equals">=</button>
-
+        
